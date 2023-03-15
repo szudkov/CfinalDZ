7c42dda712dd1896d2feff5e284350b3ca744be0
Sergy <zudkov@ya.ru>
2023-03-15 21:45:35 +0300

    64zadacha

---
+++ 64/Program.cs
@@ -0,0 +1,18 @@
+﻿int n= InputNum("Введите N: ");
+int count = 2;
+PrintNum( n, count );
+Console.WriteLine(1);
+
+void PrintNum( int n, int count )
+{
+  if (count > n) return;
+  PrintNum(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNum( string vvod ) 
+{
+  Console.WriteLine( vvod );
+  int vivod = Convert.ToInt32( Console.ReadLine() );
+  return vivod;
+}
